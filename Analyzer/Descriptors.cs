3f6d04310c7cee0ab19fa8b04c0946edc1e71e63
Daniel Cazzulino <daniel@cazzulino.com>
2023-09-05 20:49:19 -0300

    Add project files.

---
+++ Analyzer/Descriptors.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace SponsorableLib;
+
+/// <summary>
+/// Default <see cref="DiagnosticDescriptor"/>s for this sample.
+/// </summary>
+public static class Descriptors
+{
+}
---
+++ Analyzer/Generator.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.CodeAnalysis;
+
+
+namespace SourceGeneratorWarning;
+
+[Generator(LanguageNames.CSharp, LanguageNames.VisualBasic)]
+public class Generator : IIncrementalGenerator
+{
+    public static DiagnosticDescriptor WarningWithLink { get; } = new("LIB001",
+        "This is the title",
+        "This is the message",
+        "Build", DiagnosticSeverity.Warning, true,
+        "This is the description.",
+        helpLinkUri: "https://github.com/dotnet/roslyn");
+
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        context.RegisterSourceOutput(context.AnalyzerConfigOptionsProvider, (c, t) =>
+        {
+            // Simulate some issue found while generating source code.
+            c.ReportDiagnostic(Diagnostic.Create(WarningWithLink, Location.None));
+        });
+    }
+}
